803d734032aa5e521054004b0207bd34fdf19818
Everton Dutra <evertondutra02@gmail.com>
2021-03-09 15:11:47 -0300

    listando os eventos em views

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from core.models import Evento
 # Create your views here.
 
 def lista_eventos(request):
-    return render(request, 'agenda.html')
+    evento = Evento.objects.get(id=1)
+    response = {'evento': evento}
+    return render(request, 'agenda.html', response)
